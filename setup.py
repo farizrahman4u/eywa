31542fdec8b00eb30dc15e8d76c83a5a7e0004ce
Fariz Rahman <farizrahman4u@gmail.com>
2018-10-04 02:21:08 +0530

    fix setup

--- setup.py
+++ setup.py
@@ -4,18 +4,10 @@ import os
 import sys
 
 
-install_requires = ['numpy', 'dateparser', 'requests', 'numba', 'scipy']# 'mkdocs', 'mkdocs-material']
+install_requires = ['numpy', 'dateparser', 'requests', 'numba', 'scipy', 'annoy']
 
 if sys.version_info[0] == 2:
       install_requires.append('pysqlite')
-
-dependency_links = []
-
-if os.name == 'nt':
-      os.system('python -m easy_install annoy-1.8.0-py2.7-win-amd64.egg')
-else:
-      #dependency_links.append("git+ssh://git@github.com/farizrahman4u/annoy.git")
-      install_requires.append('annoy')
 
 setup(
       name='eywa',
@@ -26,7 +18,7 @@ setup(
       url='https://github.com/farizrahman4u/eywa',
       license='GNU GPL v2',
       install_requires=install_requires,
-      dependency_links=dependency_links,
+      dependency_links=[],
       packages=find_packages(),
       include_package_data=True
 )
