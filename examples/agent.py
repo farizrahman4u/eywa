f5766664acaa88bee7dc67033f6afb3cfdd20f40
ibrahiminfinite <ibrahimjkd@gmail.com>
2019-01-17 09:20:44 +0530

    formatting

--- examples/agent.py
+++ examples/agent.py
@@ -1,3 +1,27 @@
+'''
+The bot will welcome you with a greeting message once it is running.
+
+current conversatonal capablities.
+
+1. greetings      - Hi , hello etc
+2. cab booking    - book a cab to <place>
+3. weather        - what is the weather in <place>
+4. date           - what is the date today
+
+
+Sample conversation
+
+Agent : Hello
+        what would you like me to do ?
+you   : what is the weather in New York           
+Agent : The weather in New_York is warm
+you   : book a cab to NYC
+Agent : Booking a cab for NYC
+you   : what is the date today
+Agent : Today is 2019-01-17
+'''
+
+
 from eywa.nlu import Classifier
 from eywa.nlu import EntityExtractor
 import random
@@ -6,10 +30,8 @@ import datetime
 
 
 '''
-Classifer for predicting the class of user input
+Classifer, predicts the class of user input
 '''
-
-#training data for classifier
 conv_samples = {
   'greetings' : ['Hi', 'hello', 'How are you', 'hey there', 'hey'],
 
@@ -17,66 +39,54 @@ conv_samples = {
 
   'weather'   : ['what is the weather in tokyo', 'weather germany', 'what is the weather like in kochi'],
 
-  'datetime'      : ['what day is today', 'todays date', 'what time is it now', 'time now', 'what is the time']
-}
+  'datetime'  : ['what day is today', 'todays date', 'what time is it now', 'time now', 'what is the time']}
 
-
-# Eywa classifier object
 clf  = Classifier()
-
-for key in conv_samples.keys():
-
-	#training the classifier object on the sample inputs
-	clf.fit(conv_samples[key],key)
+for key in conv_samples:
+    clf.fit(conv_samples[key],key)
 
 
 '''
-Entitiy Extractor , gets the required entities from the input
+Entitiy Extractor, gets the required entities from the input
 '''
-
-
-#training data for Entitiy Extractor
 x_weather = ['what is the weather in tokyo', 'weather germany', 'what is the weather like in kochi']
 y_weather = [{'intent': 'weather', 'place': 'tokyo'}, {'intent': 'weather', 'place': 'germany'},
                   {'intent': 'weather', 'place': 'kochi'}]
-
-x_taxi = ['book a cab to kochi ', 'need a ride to delhi', 'find me a cab for manhattan',
-               'call a taxi to calicut']
-y_taxi = [ {'service': 'cab', 'destination': 'kochi'}, {'service': 'ride', 'destination' : 'delhi'}, 
-                {'service': 'cab', 'destination' : 'manhattan'}, {'service': 'taxi', 'destination' : 'calicut'}
-              ]
-
-x_greeting = ['Hii', 'helllo', 'Howdy', 'hey there', 'hey', 'Hi']
-y_greeting = [{'greet': 'Hii'}, {'greet': 'helllo'}, {'greet': 'Howdy'}, {'greet': 'hey'}, {'greet': 'hey'}, {'greet': 'Hi'}]
-
-x_datetime = ['what day is today', 'date today', 'what time is it now', 'time now']
-y_datetime = [ {'intent' : 'day', 'target': 'today'}, {'intent' : 'date', 'target': 'today'},
-                    {'intent' : 'time', 'target': 'now'},{'intent' : 'time', 'target': 'now'}
-                  ]
-
-
-
-#training the extractors on the data
 
 ex_weather = EntityExtractor()
 ex_weather.fit(x_weather, y_weather)
 
 
+x_taxi = ['book a cab to kochi ', 'need a ride to delhi', 'find me a cab for manhattan',
+               'call a taxi to calicut']
+y_taxi = [ {'service': 'cab', 'destination': 'kochi'}, {'service': 'ride', 'destination' : 'delhi'}, 
+                {'service': 'cab', 'destination' : 'manhattan'}, {'service': 'taxi', 'destination' : 'calicut'}]
+
 ex_taxi = EntityExtractor()
 ex_taxi.fit(x_taxi, y_taxi)
 
+
+
+x_greeting = ['Hii', 'helllo', 'Howdy', 'hey there', 'hey', 'Hi']
+y_greeting = [{'greet': 'Hii'}, {'greet': 'helllo'}, {'greet': 'Howdy'}, {'greet': 'hey'}, {'greet': 'hey'}, {'greet': 'Hi'}]
+
 ex_greeting = EntityExtractor()
 ex_greeting.fit(x_greeting, y_greeting)
+
+
+x_datetime = ['what day is today', 'date today', 'what time is it now', 'time now']
+y_datetime = [ {'intent' : 'day', 'target': 'today'}, {'intent' : 'date', 'target': 'today'},
+                    {'intent' : 'time', 'target': 'now'},{'intent' : 'time', 'target': 'now'}]
 
 ex_datetime = EntityExtractor()
 ex_datetime.fit(x_datetime, y_datetime)
 
 
 _extractors =  { 'taxi':ex_taxi,
-                      'weather':ex_weather, 
-                      'greetings':ex_greeting,
-                      'datetime':ex_datetime
-                    }
+                 'weather':ex_weather, 
+                 'greetings':ex_greeting,
+                 'datetime':ex_datetime
+               }
 
 
 
@@ -85,38 +95,46 @@ _extractors =  { 'taxi':ex_taxi,
 Response logic 
 '''
 
-def get_response(qclass, qentities):
+def get_response(uquery):
 
-	responses = { }
+    responses = { }
 
-	i = random.randint(0,2)
+    i = random.randint(0,2)
+
+    # Predict the class of the query.
+    q_class = clf.predict(uquery) 
+
+    # Run entity extractor of the predicted class on the query.
+    q_entities = _extractors[q_class].predict(uquery)
 
 
-	if qclass == 'weather':
-		responses['weather' ] = 'The '+qentities['intent']+' in '+qentities['place']+ ' is warm'
+    # Get response based on the class of the query.
+    if q_class == 'weather':
+        responses['weather' ] = 'The '+q_entities['intent']+' in '+q_entities['place']+ ' is warm'
 
-		def get_weather():
-			# weather api call
-			pass
+        def get_weather():
+            # Weather api call.
+            pass
 
-	if qclass == 'taxi':
-		responses['taxi'    ] =  'Booking a '+qentities['service']+ ' for '+qentities['destination']
+    if q_class == 'taxi':
+        responses['taxi'    ] =  'Booking a '+q_entities['service']+ ' for '+q_entities['destination']
 
-		def get_taxi():
-			# uber/ola api
-			pass
+        def get_taxi():
+            # Uber/Ola api.
+            pass
 
-	if qclass == 'datetime':
-		responses['datetime'    ] =  'Today is '+str(datetime.datetime.today()).split(' ')[0]
-		def get_dateime():
-			#calender api
-			pass
+    if q_class == 'datetime':
+        responses['datetime'    ] =  'Today is '+str(datetime.datetime.today()).split(' ')[0]
+        def get_dateime():
+            # Calender api.
+            pass
 
-	if qclass == 'greetings':
-		responses['greetings'] =  ['Hey', 'Hi there', 'Hello'][i] + ['\nwhat would you like me to do ?', '', '\nwhat would you like me to do ?',][i]
+    if q_class == 'greetings':
+        responses['greetings'] =  ['Hey', 'Hi there', 'Hello'][i] + ['\nwhat would you like me to do ?', '', '\n        what would you like me to do ?',][i]
 
-	return responses[qclass]
+    return 'Agent : '+responses[q_class]
 
+    
 
 
 
@@ -125,28 +143,13 @@ Conversation loop
 '''
 
 if __name__ == '__main__':
+    # Greeting user on startup.
+    print(get_response('Hi'))
 
-	def talker(uquery):
-
-		#predict the class of the query
-		q_class = clf.predict(uquery) 
-
-		#run entity extractor of the predicted class on the query
-		q_entities = _extractors[q_class].predict(uquery)
-
-		#get response fro the query
-		response = get_response(q_class,q_entities)
-		
-
-		return response
-
-
-	print(talker('Hi'))
-
-	while True:
-
-		uquery = input()
-		if uquery == 'bye':
-			break
-
-		print(talker(uquery))
+    # Conversation loop.
+    while True:
+        uquery = input('you   : ')
+        if uquery == 'bye':
+            break
+        response  = get_response(uquery)
+        print(response)
