d13b540e7f7b98419e0e1c94f522a9cb3fac35a7
V Mohammed Ibrahim <12377945+ibrahiminfinite@users.noreply.github.com>
2019-04-21 17:19:44 +0530

    More tests for classifier (#41)
    * Travis build fixed
    
    * add classifier tests
    
    * add classifier tests
    
    * formatting

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -7,22 +7,29 @@ class TestClassifier:
     def test_classifier_basic(self):
         x_hotel = ['book a hotel', 'need a nice place to stay', 'any motels near by']
         x_weather = ['what is the weather like', 'is it hot outside']
-
-        clf = Classifier()
-        clf.fit(x_hotel, 'hotel')
-        clf.fit(x_weather, 'weather')
-
-        assert clf.predict('will it rain today') == 'weather'
-        assert clf.predict('find a place to stay') == 'hotel'
-
-        x_greetings = ['hi', 'hello', 'hello there', 'hey']
-        x_not_greetings = x_hotel + x_weather
-
+        x_place = ['which place is this', 'where are we', 'where are you going', 'which place is it']
+        x_name = ['What is your name', 'Who are you', 'What do i call you', 'what are you called']
+        
+        clf1 = Classifier()
         clf2 = Classifier()
-        clf2.fit(x_greetings, 'greetings')
-        clf2.fit(x_not_greetings, 'not_greetings')
-        assert clf2.predict('flight information') == 'not_greetings'
-        assert clf2.predict('hey there') == 'greetings'
+        clfs = [clf1, clf2]
+        for cl in clfs:
+            cl.fit(x_hotel, 'hotel')
+            cl.fit(x_weather, 'weather')
+            cl.fit(x_place, 'place')
+            cl.fit(x_name, 'name')
+
+        assert clf1.predict('will it rain today') == 'weather'
+        assert clf1.predict('find a place to stay') == 'hotel'
+        assert clf1.predict('where am I') == 'place'
+        assert clf1.predict('may i know your name') == 'name'
+
+        # Testing return probablities for each of the above tests
+        assert clf1.predict('will it rain today', True) == clf2.predict('will it rain today', True)
+        assert clf1.predict('find a place to stay', True) == clf2.predict('find a place to stay', True)
+        assert clf1.predict('where am I',True) == clf2.predict('where am I',True)
+        assert clf1.predict('may i know your name', True) == clf2.predict('may i know your name', True)
+
 
     def test_classifier_serialization(self):
         x_hotel = ['book a hotel', 'need a nice place to stay', 'any motels near by']
