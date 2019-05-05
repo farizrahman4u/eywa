52ac13e9070404bc216788d5af04d256d217356b
true-source <christeejacobs@gmail.com>
2019-05-05 22:48:21 +0530

    pep8/ comments

--- eywa/nlu/entity_extractor_utils.py
+++ eywa/nlu/entity_extractor_utils.py
@@ -1,18 +1,18 @@
+"""Checks, utilities for entity_extractor
+"""
 
 def check_inputs_targets_consistency(inputs, targets):
-
-	if type(inputs) != list:
-		raise TypeError('Input must be of type list.' 
-						'Got %s instead'%type(inputs))
-	if type(targets) != list:
-		raise TypeError('Target must be of type list.' 
-						'Got %s instead'%type(targets))
-	if len(inputs) != len(targets):
-		raise ValueError('Inputs should have the same'
-						' number of samples as targets.'
-						'Found ' + str(len(inputs)) +
-						' input samples and ' + str(len(targets)) +
-						' target samples')
-
+    if type(inputs) != list:
+        raise TypeError('Input must be of type list.' 
+                        'Got %s instead'%type(inputs))
+    if type(targets) != list:
+        raise TypeError('Target must be of type list.' 
+                        'Got %s instead'%type(targets))
+    if len(inputs) != len(targets):
+        raise ValueError('Inputs should have the same'
+                        ' number of samples as targets.'
+                        'Found ' + str(len(inputs)) +
+                        ' input samples and ' + str(len(targets)) +
+                        ' target samples')
 
 
