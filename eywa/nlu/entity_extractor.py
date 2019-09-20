9fffee7ad6d005b1cb6c973fcea248eb4bf2127c
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-09-20 23:30:21 +0530

    Update eywa/nlu/entity_extractor.py

--- eywa/nlu/entity_extractor.py
+++ eywa/nlu/entity_extractor.py
@@ -34,7 +34,7 @@ class EntityExtractor(object):
         X: Input utterance(s). It could be:
             - `str` (or `list` thereof)
             - `Document` instance (or `list` thereof)
-        Y: Target labels. 
+        Y: Target values. 
         - a `dict` mapping from from entity name (`str`)
           to entity value (`str`)(or list thereof).
           The entity names should be same throughout all `dict` elements and
