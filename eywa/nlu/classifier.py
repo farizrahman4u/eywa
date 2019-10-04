55b312dad88df4485b9d505147d24057844d35b9
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-10-04 15:29:24 +0530

    docstrings on entityextractor (#57)
    * Update eywa/nlu/entity_extractor.py
    
    * docstrings for entityextractor
    
    * updating docstrings for fit ,compile ,predict and evaluate methods
    
    * docs on method of entityextractor class
    
    * docstrings for fit ,predict similarity and evaluate
    
    * docstring for fit inside entityextractor
    
    * fit method update entityextractor
    
    * Update eywa/nlu/entity_extractor.py
    
    * Update eywa/nlu/entity_extractor.py
    
    * Update entity_extractor.py
    
    * Update eywa/nlu/entity_extractor.py
    
    * Update eywa/nlu/entity_extractor.py
    
    * Update eywa/nlu/entity_extractor.py
    
    * Update eywa/nlu/entity_extractor.py
    
    * Update eywa/nlu/entity_extractor.py
    
    * updated docstring for predict
    
    * Update eywa/nlu/entity_extractor.py
    
    * updated predict and evaluate method
    
    * updated remaining methods of entityextractor
    
    * docstrings for some methods
    
    * updated docstrings for Pattern class and init
    
    * updated docstring for init
    
    * Update eywa/nlu/pattern.py
    
    * content for hosting

--- eywa/nlu/classifier.py
+++ eywa/nlu/classifier.py
@@ -8,8 +8,15 @@ import numpy as np
 
 
 class Classifier(Switch):
+    """
+    
+    Predicts the class of user input.
+    
+    """
 
     def __init__(self, *args, **kwargs):
+        
+        
         super(Classifier, self).__init__(*args, **kwargs)
         self.X = []
         self.Y = []
@@ -27,11 +34,57 @@ class Classifier(Switch):
         return list(self.data.keys())
 
     def fit(self, X, Y):
-        if type(X) in (str, Document):
+        """
+        Trains the model on given data.
+
+        # Arguments
+        X: Input utterance(s). It could be:
+            - `str` (or `list` thereof)
+            - `Document` instance (or `list` thereof)
+        Y: Target labels. `str` (or `list` thereof). If `list`,
+        number of items in Y should be either 1 or equal
+        to number of utterances in X.
+
+        # Example
+
+        Train a `Classifier` to classify a given utterance
+        to 2 classes: "greeting" and "bye":
+
+        Method 1 - fit on individual utterances:
+        ```python
+        clf = Classifier()
+        clf.fit('hi', 'greeting')
+        clf.fit('good bye', 'bye')
+        clf.fit('hello', 'greeting')
+        clf.fit('see you later', 'bye')
+        ```
+        Method 2 - fit on list of utterances:
+        ```python
+        clf = Classifier()
+        greetings = ['hi', 'hello', 'hey']
+        bye = ['bye', 'good bye', 'see you later']
+        clf.fit(greetings, 'greeting')
+        clf.fit(bye, 'bye')
+        ```
+        Method 3 - fit on list of utterances and labels:
+        ```python
+        clf = Classifier()
+        input_data = ['hi', 'good bye', 'hello', 'hey', 'see you later']
+        target_labels = ['greeting', 'bye', 'greeting', 'greeting', 'bye']
+        clf.fit(input_data, target_labels)
+        ```
+        """
+        
+
+        if isinstance(X, (str, Document)):
             X = [X]
+        if isinstance(Y, str):
             Y = [Y]
-        if type(Y) not in (list, tuple):
-            Y = [Y] * len(X)
+        assert isinstance(X, (list, tuple))
+        assert isinstance(Y, (list, tuple))
+        if len(Y) == 1:
+            # broadcasting
+            Y = Y * len(X)
         assert len(X) == len(Y), "Different number of samples in X and Y."
         X_app = self.X.append
         Y_app = self.Y.append
@@ -63,8 +116,36 @@ class Classifier(Switch):
         return scores
 
     def predict(self, x, return_scores=False):
+        """Predicts labels for given input utterance(s)
+
+        # Arguments
+        x: Input utterance(s). It could be:
+            - `str` (or `list`/`tuple` thereof)
+            - `Document` instance (or `list`/`tuple` thereof)
+        `return_scores`: `bool`. Default `False`.
+        If `True`, returns confidence for each class per utterance.
+        Else, returns label for class with highest confidence per utterance.
+
+        # Returns
+        if `return_scores` is `True`:
+            if `x` is a single utterance:
+                Returns a `list` of `tuple`s of the
+                form (label, confidence) for each class,
+                sorted by decreasing order of confidence.
+            if `x` is a `list`/`tuple` of utterances:
+                Returns a `list` of results with 1 result per
+                utterance. Each result will be a `list` of
+                `tuple`s of the form (label, confidence) for
+                each class, sorted by decreasing order of confidence.
+        if `return_scores` is `False`:
+            if `x` is a single utterance:
+                Returns the predicted label as `str`.
+            if `x` is a `list`/`tuple` of utterances:
+                Returns the predicted labels for utterances as `list` of
+                `str`.
+        """
         if type(x) in (list, tuple):
-            return type(x)([self.predict(i, return_scores) for i in x])
+            return [self.predict(i, return_scores) for i in x]
         if type(x) is not Document:
             x = Document(x)
         classes = list(self.data.keys())
@@ -76,8 +157,6 @@ class Classifier(Switch):
         return classes[np.argmax(scores.numpy())]
 
     def _similarity(self, x1, x2):
-        # if x1 == x2:
-        #    return 1
         if len(x1) == 0 or len(x2) == 0:
             return 0.
         weights = self.weights
@@ -95,9 +174,41 @@ class Classifier(Switch):
         return score * 0.25
 
     def evaluate(self, X=None, Y=None):
+        """Evaluates the Classfier on given data.
+
+        Either both `X` and `Y` arguments should be provided
+        or both of them should be left unspecified (`None`).
+        If left unspecified, the cumilative data used to train
+        the `Classifier` will be used for evaluation .
+
+        # Arguments
+        X: Input utterance(s). It could be:
+            - `str` (or `list` thereof)
+            - `Document` instance (or `list` thereof)
+        Y: Target labels. str (or list thereof). If list,
+        number of items in Y should be either 1 or equal
+        to number of utterances in X.
+
+        # Returns
+        `tuple` of error(`float`) and accuracy(`float`)
+        """
+        arg_set = set([X, Y])
+        if len(arg_set) > 1 and None in arg_set:
+            raise Exception('Both or none of X and Y should be provided.')
         if X is None:
             X = self.X
             Y = self.Y
+        else:
+            if isinstance(X, (str, Document)):
+                X = [X]
+            if isinstance(Y, str):
+                Y = [Y]
+            assert isinstance(X, (list, tuple))
+            assert isinstance(Y, (list, tuple))
+            if len(Y) == 1:
+                # broadcasting
+                Y = Y * len(X)
+            assert len(X) == len(Y), "Different number of samples in X and Y."
         acc = 0.
         err = 0.
         Y_pred = self.predict(X, True)
@@ -109,6 +220,12 @@ class Classifier(Switch):
         return err, acc
 
     def serialize(self):
+        """Serializes the `Classifier` object to a json
+        friendly config.
+
+        # Returns
+        `dict`
+        """
         config = super(Classifier, self).serialize()
         config['X'] = [str(x) for x in self.X]
         config['Y'] = self.Y[:]
@@ -117,6 +234,14 @@ class Classifier(Switch):
 
     @classmethod
     def deserialize(cls, config):
+        """Deserializes a `Classifer` config to a `Classifier` instance.
+
+        # Arguments
+        config: `dict`. `Classifier` config (generated by `Classifier.serialize`).
+
+        # Returns
+        `Classifier` instance
+        """
         weights = config.pop('weights')
         X, Y = config.pop('X'), config.pop('Y')
         clf = super(Classifier, cls).deserialize(config)
@@ -125,12 +250,23 @@ class Classifier(Switch):
         return clf
 
     def set_weights(self, weights):
+        """Sets weights of the `Classifier` to given
+        values.
+
+        # Arguments
+        weights: `list` of numpy arrays
+        """
         assert isinstance(weights, list)
         assert len(weights) == len(self.weights)
         for (w_in, w_curr) in zip(weights, self.weights):
                 w_curr.assign(w_in)
 
     def get_weights(self):
+        """Returns weights of the `Classifier`.
+
+        # Returns
+        `list` of numpy arrays
+        """
         return [w.numpy() for w in self.weights]
 
     def switch_f(self, inputs):
@@ -147,4 +283,3 @@ class Classifier(Switch):
         elif blame.blame_type == BlameType.NEGATIVE:
             for g, w in zip(self.grads[self.value], self.weights):
                 w.assign_sub(g * 0.05)
-
